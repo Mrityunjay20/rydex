95bde2336df852944b95c54f4a4b8aae1bc630e1
Mrityunjay20 <mrityunjay.shrivastava20@gmail.com>
2026-07-16 00:15:29 +0530

    fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Script from "next/script";
-import "./globals.css";
+import "./globals.css"; 
 import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({
